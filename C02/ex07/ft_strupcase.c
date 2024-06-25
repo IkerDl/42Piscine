dea57d6657f44bbae24c831f82c1c6101b19b170
IkerDl <123637459+IkerDl@users.noreply.github.com>
2024-06-25 17:30:11 +0200

    Ex07 completed

---
+++ C02/ex07/ft_strupcase.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+int	main(void)
+{
+	char	str[6];
+
+	str[0] = 'H';
+	str[1] = 'e';
+	str[2] = 'l';
+	str[3] = 'l';
+	str[4] = 'o';
+	str[5] = '\0';	
+
+	ft_strupcase(str);
+	printf("Result with Hello is %s", str);
+	return(0);
+}
